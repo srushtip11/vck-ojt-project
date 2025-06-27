89fafa2eb8c82c8b1d8cb16bee56284b0c4db089
srushti <srushtipatil76799@gmail.com>
2025-06-27 10:33:53 +0530

    srushti third commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,29 @@ import HomePage from "./pages/HomePage";
 import AboutPage from "./pages/AboutPage";
 import ContactPage from "./pages/ContactPage";
 import CoursePage from "./pages/CoursesPage";
-import './styles/pages.css'
+import './styles/Pages.css'
 import ChatbotComponent from "./components/Chatbot/ChatbotComponent";
+import DeveloperInfoPopup from "./components/DeveloperInfo/DeveloperInfoPopup";
+import { useState } from "react";
 
 const App = () => {
+  const [showPopup, setShowPopup] = useState(true);
+  const handleClosePopup = () => {
+    setShowPopup(false);
+  };
   return(
+
     <div>
+      <div>
+        {/* Your main application content */}
+        <DeveloperInfoPopup
+          show={showPopup}
+          onClose={handleClosePopup}
+          studentName="Srushti sadashiv patil"
+          studentPhotoUrl="/images/SRUSHTI.jpg" // Path to their photo
+          uniqueMessage="Learned so much during this OJT! This app showcases my independent coding and deployment skills"
+        />
+      </div>
       <Router>
         <Routes>
           <Route path="/" element={<HomePage/>}/>
